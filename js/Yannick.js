1af018dc3b491b1ebab3b776829334cead6bd6fb
SylvexX <108277123+SylvexX@users.noreply.github.com>
2023-01-21 04:27:47 +0100

    Mijn Quiz 1.0
    Mijn quiz geupdate, alle vragen gemaakt, in het engels, En mijn informatie aangepast

--- js/Yannick.js
+++ js/Yannick.js
@@ -12,42 +12,65 @@ let availibleQuestions = 0
 
 let questions = [
     {
-        question: 'what is 1 + 1',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '8',
+        question: 'Who captured Miquella during the battle of Aeonia?',
+        choice1: 'Mohg, Lord of Blood',
+        choice2: 'Godfrey, First Elden lord',
+        choice3: 'Maliketh, the Black Blade',
+        choice4: 'Morgott, the Elden King',
         answer: 1,
     },
     {
-        question: 'what is 2 + 2',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '8',
+        question: 'What animal represents Godfrey, the first Elden Lord?',
+        choice1: 'Stormhawk',
+        choice2: 'Lion',
+        choice3: 'Wold',
+        choice4: 'Dog(turtle)',
         answer: 2,
     },
     {
-        question: 'what is 4 + 4',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '8',
+        question: 'Who stole a fragment of the Rune of Death?',
+        choice1: 'Radagon of the Golden Order',
+        choice2: 'Marika the Eternal',
+        choice3: 'Rennala, Queen of the Full Moon',
+        choice4: 'Ranni, Lunar Princess',
         answer: 4,
     },
     {
-        question: 'what is 3 + 3',
-        choice1: '2',
-        choice2: '4',
-        choice3: '6',
-        choice4: '8',
+        question: 'What was the name of Godfrey`s elite group of soldiers?',
+        choice1: 'The Black Blades',
+        choice2: 'The Godskin',
+        choice3: 'The Cruicible Knights',
+        choice4: 'The Golden Order',
         answer: 3,
     },
-
+    {
+        question: 'Who shattered the Elden Ring?',
+        choice1: 'Rennala, Queen of the Full Moon',
+        choice2: 'Radagon, The Second Elden Lord',
+        choice3: 'Marika, the Eternal',
+        choice4: 'Godfrey, The First Elden Lord',
+        answer: 3,
+    },
+    {
+        question: 'Who challenged the Golden Order?',
+        choice1: 'The Carian Royal Family',
+        choice2: 'The Ancient Dragons and Northern Giants',
+        choice3: 'The Storm Lord of Stormveil',
+        choice4: 'All of the above',
+        answer: 4,
+    },
+    {
+        question: 'Why is Caelid covered in scarlet rot?',
+        choice1: 'Malenia unleashed it upon the land during her battle against Radahn',
+        choice2: 'Radahn accidentally unleashed it upon the land when returning from battle',
+        choice3: 'Marika cursed the land after Radahn went to war against her',
+        choice4: 'After The Shattering, rot began to consume Caelid without reasony',
+        answer: 1,
+    },
 ]
 
 const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const MAX_QUESTIONS = 7
 
 startGame = () => {
     questionCounter = 0
