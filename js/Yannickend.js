6147dea09489f8f823f619479d05d90073973ae5
Aurora23134 <108567188+Aurora23134@users.noreply.github.com>
2023-01-25 11:03:00 +0100

    Yannick quiz score

---
+++ js/Yannickend.js
@@ -0,0 +1,32 @@
+const username = document.querySelector('#username')
+const savScoreBtn = document.querySelector('#saveScoreBtn')
+const finalscore = document.querySelector('#finalscore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+const highscores = JSON.parse(localStorage.getItem('highScores')) || []
+
+finalscore.innerTEXT = mostRecentScore
+
+username.addEventListener('keyup', () => {
+    savScoreBtn.disabled = !username.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    }
+
+    highscores.push(score)
+
+    highscores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highscores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highscores))
+    window.location.assign('../quiz/highscoreYannick.html')
+}
